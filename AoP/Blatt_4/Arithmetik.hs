c451549cf6a39cdf0d2e5436da32a7110118d9e2
Jean-Pierre Kasperschinski <Jean-Pierre.Kasperschinski@b-tu.de>
2024-09-03 14:50:34 +0200

    fix

--- AoP/Blatt_4/Arithmetik.hs
+++ AoP/Blatt_4/Arithmetik.hs
@@ -9,14 +9,9 @@ data Ausdruck = Konstante Integer
     |Quotient Ausdruck Ausdruck
     deriving (Show)
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
 
->>>>>>> 03228944d81073dcd6065edf59aa9266d8b88b0c
---3
->>>>>>> refs/remotes/origin/master
+
+
 ausdruckNachString :: Ausdruck -> String
 ausdruckNachString (Konstante n) = show n
 ausdruckNachString (Variable v) = v
@@ -26,30 +21,24 @@ ausdruckNachString (Produkt e1 e2) = "(" ++ ausdruckNachString e1 ++ " * " ++ au
 ausdruckNachString (Quotient e1 e2) = "(" ++ ausdruckNachString e1 ++ " / " ++ ausdruckNachString e2 ++ ")"
 --Hier werden die veschiedenen Funktionen angegeben, diese brauch man für die verschiedenen Fälle
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
+
 --4
 belegungVonVariable :: String -> [(String, Integer)] -> Integer
 belegungVonVariable a [] = error (a ++ " ist nicht deffiniert") --Abruchsfunktion, kommt auto., wenn alle Tupel überprüft wurden und kein Element in der Liste übrig bleibt
 belegungVonVariable a ((b, c): xs)
     | a == b = c --überprüft ob der String aus der Eingabe sich mit dem aus dem Tupel gleicht, wenn ja wird der zugehörige Int ausgegeben 
     | a /= b = belegungVonVariable a xs --ist da um in der Liste vorranzuschreiten
-=======
 
 
 --4
->>>>>>> refs/remotes/origin/master
-belegungVonVariable :: String -> [(String, Integer)] -> Integer 
-belegungVonVariable a [] = error (a ++ " ist nicht deffiniert") --Abruchsfunktion, kommt auto., wenn alle Tupel überprüft wurden und kein Element in der Liste übrig bleibt
-belegungVonVariable a ((b, c): xs)
-    | (a == b) = c --überprüft ob der String aus der Eingabe sich mit dem aus dem Tupel gleicht, wenn ja wird der zugehörige Int ausgegeben 
-<<<<<<< HEAD
-    | (a /= b) = (belegungVonVariable a xs) --ist da um in der Liste vorranzuschreiten 
-=======
-    | (a /= b) = (belegungVonVariable a xs) --ist da um in der Liste vorranzuschreiten
-    
+belegungVonVariable2 :: String -> [(String, Integer)] -> Integer
+belegungVonVariable2 a [] = error (a ++ " ist nicht deffiniert") --Abruchsfunktion, kommt auto., wenn alle Tupel überprüft wurden und kein Element in der Liste übrig bleibt
+belegungVonVariable2 a ((b, c): xs)
+    | a == b = c --überprüft ob der String aus der Eingabe sich mit dem aus dem Tupel gleicht, wenn ja wird der zugehörige Int ausgegeben 
+
+    | a /= b = belegungVonVariable2 a xs --ist da um in der Liste vorranzuschreiten 
+
+    | a /= b = belegungVonVariable2 a xs --ist da um in der Liste vorranzuschreiten
+
 --5
-auswerten
->>>>>>> 03228944d81073dcd6065edf59aa9266d8b88b0c
->>>>>>> refs/remotes/origin/master
+
